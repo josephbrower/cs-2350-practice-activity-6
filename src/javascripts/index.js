b762cca065c554faa73282a51f481e3e427a1510
Joseph Brower <josephbrower@mail.weber.edu>
2020-03-20 14:20:10 -0600

    Fixed the issue

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,19 @@
 // Required by Webpack - do not touch
+
 require.context('../', true, /\.(html|json|txt|dat)$/i)
 require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+console.log("connected!");
+
+let name = document.getElementById("submit_button");
+let namefld = document.getElementById("namefld");
+
+function submitText(){
+    alert("welcome " + namefld.value);
+}
+
+name.addEventListener("click", submitText);
